723dfb6fe62f3769e0ab36b507146d9651e47a4c
AdrielGraminho <adrielgraminho@gmail.com>
2021-03-11 12:24:58 -0300

    cursos splenda

---
+++ Curso2 Java-8/src/unidade6/JAppletApp.java
@@ -0,0 +1,13 @@
+package unidade6;
+import javax.swing.JApplet;
+import javax.swing.JOptionPane;
+
+public class JAppletApp extends JApplet{
+	
+	@override
+	private void init() {
+
+		JOptionPane.showMessageDialog(null, "Curso de java");
+	}
+
+}
