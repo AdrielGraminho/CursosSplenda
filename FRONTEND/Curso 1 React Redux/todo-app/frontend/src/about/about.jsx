c365ba0f5926ace8b53c755221942883d2df00cb
AdrielGraminho <adrielgraminho@gmail.com>
2021-04-29 08:52:00 -0300

    Cursos splenda

--- FRONTEND/Curso 1 React Redux/todo-app/frontend/src/about/about.jsx
+++ FRONTEND/Curso 1 React Redux/todo-app/frontend/src/about/about.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
+
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod rerum nesciunt placeat, tempora quia quaerat maiores consectetur id ratione ullam. Nesciunt odit soluta perspiciatis voluptates doloremque aspernatur ad voluptate! Eos.</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus repudiandae iusto est. Commodi laboriosam architecto corrupti aspernatur id dignissimos maxime provident, qui cum ducimus iusto libero ex veniam inventore eaque.</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum reiciendis perferendis quo aliquam! Doloribus, aliquid voluptatibus. Quod aut error, quia saepe temporibus autem laboriosam quis recusandae, earum voluptas quo odit!</p>
     </div>
 )
