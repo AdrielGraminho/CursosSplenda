168c11978b2efa53121b71a32cdd012f3791ae04
AdrielGraminho <adrielgraminho@gmail.com>
2021-05-01 14:46:55 -0300

    Cursos splenda

--- FRONTEND/Curso 1 React Redux/todo-app/frontend/src/todo/todoList.jsx
+++ FRONTEND/Curso 1 React Redux/todo-app/frontend/src/todo/todoList.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import {bindActionCreators} from 'redux'
+
 import IconButton from '../template/iconButton'
+import { markAsDone, markAsPending, remove } from './todoActions'
 
-
-export default props => {
+const TodoList = props => {
 
         const renderRows = () => {
             const list = props.list || []
@@ -12,11 +15,11 @@ export default props => {
                     <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
                     <td>
                         <IconButton style="success" icon='check' hide={todo.done}
-                        onClick={() => props.handleMarkAsDone(todo)}></IconButton>
+                        onClick={() => props.markAsDone(todo)}></IconButton>
                         <IconButton style="warning" icon='undo' hide={!todo.done}
-                        onClick={() => props.handleMarkAsPending(todo)}></IconButton>
+                        onClick={() => props.markAsPending(todo)}></IconButton>
                         <IconButton style='danger' icon='trash-o'
-                            onClick={() => props.handleRemove(todo)}>
+                            onClick={() => props.remove(todo)}>
 
                             </IconButton>
                     </td>
@@ -37,3 +40,8 @@ export default props => {
         </table>
     )
 }
+
+const mapStateToProps = state => ({ list: state.todo.list })
+const mapDispatchToProps = (dispatch) => bindActionCreators({ markAsDone, markAsPending, remove }, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
