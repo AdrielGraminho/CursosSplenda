5e376f08f690990ef733b3aaac74cb936de7dccb
AdrielGraminho <adrielgraminho@gmail.com>
2021-05-02 09:02:49 -0300

    Cursos splenda

---
+++ FRONTEND/Curso 1 React Redux/my-money-app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2021
+         <a href="https:www.adrieldev.com.br" target='_blank'> Adrieldev</a>
+        </strong>
+
+    </footer>
+)
